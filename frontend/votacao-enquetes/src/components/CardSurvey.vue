bf0706d957b9d1c43ab86d2d0d2fde3343e5564a
DanielZanad <daniel.zanad.dz@gmail.com>
2022-07-15 13:19:59 -0300

    add: componenets CardSurvey e SectionSurvey

---
+++ frontend/votacao-enquetes/src/components/CardSurvey.vue
@@ -0,0 +1,17 @@
+<template>
+  <article class="main-article card-survey">
+    <h3>Titulo da enquete</h3>
+  </article>
+</template>
+
+<script>
+import {ref} from 'vue';
+
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
---
+++ frontend/votacao-enquetes/src/components/SectionSurvey.vue
@@ -0,0 +1,20 @@
+<template>
+  <section class="main-section survey">
+    <article>
+      <h2>Para iniciar</h2>
+    </article>
+    <CardSurvey />
+  </section>
+</template>
+
+<script>
+import CardSurvey from './CardSurvey.vue';
+
+export default {
+  components: { CardSurvey }
+}
+</script>
+
+<style>
+
+</style>
